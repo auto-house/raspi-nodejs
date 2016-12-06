554555ec4aa5e19bafc8871b6af76fafd615801e
Vítor Barros <vba2@cin.ufpe.br>
2016-12-06 17:18:10 -0300

    Fix

--- controllersHW/ldr.js
+++ controllersHW/ldr.js
@@ -10,6 +10,8 @@ Ldr.prototype.ldr = function(pin) {
 	var count = 0;
 	var ret = 0;
 
+	sleep.sleep(2);
+
 	pin.writeSync(0);
 	sleep.usleep(100000);
 
@@ -19,18 +21,12 @@ Ldr.prototype.ldr = function(pin) {
 		count++;
 	}
 
-	if (count < 50)
-		ret = 1;
-	else if (count >= 50 && count <= 100)
-		ret = 2;
-	else if (count >= 100 && count <= 150)
+	if (count < 120)
 		ret = 3;
-	else if (count >= 150 && count <= 200)
-		ret = 4;
-	else if (count >= 200 && count <= 250)
-		ret = 5;
-	else if (count >= 250)
-		ret = 6;
+	else if (count >= 120 && count < 1000)
+		ret = 2;
+	else if (count >= 1000)
+		ret = 1;
 
 	return ret;
 
